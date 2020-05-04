5af80852609d0b075773909dda7d329f7a513133
Velupriya <velupriya@thinkbridge.in>
2020-05-04 09:18:47 +0530

    added autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,16 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormLoginComponent } from './reactive-form-login/reactive-form-login.component';
-
+import { AutocompleteComponent } from './reactive-form-login/autocomplete/autocomplete.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     // MatSelectModule,
     TemplateDrivenFormComponent,
-    ReactiveFormLoginComponent
+    ReactiveFormLoginComponent,
+    AutocompleteComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +32,7 @@ import { ReactiveFormLoginComponent } from './reactive-form-login/reactive-form-
     MatSelectModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    MatAutocompleteModule,
     AppRoutingModule
   ],
   providers: [],
