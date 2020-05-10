3ef46be3ffdb3f9511a8e44773a43e67c97e484f
Velupriya <velupriya@thinkbridge.in>
2020-05-10 09:52:00 +0530

    nav-menu

--- src/app/Investments/investment-routing.module.ts
+++ src/app/Investments/investment-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormLoginComponent } from './reactive-form-login/reactive-form-login.component';
-import { AutocompleteComponent } from './reactive-form-login/autocomplete/autocomplete.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
-import { DynamicReactFormComponent } from './dynamic-react-form/dynamic-react-form.component';
-import { LoginComponent } from './login/login.component';
 import { CommonModule } from '@angular/common';
 import { InitialInvestmentComponent } from './initial-investment/initial-investment.component';
 import { PassiveInvestmentComponent } from './passive-investment/passive-investment.component';
